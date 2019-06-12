ed8212685f0157b90b200578bcfe3f85881e2883
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2019-06-12 13:57:36 -0400

    Webscraping Test 1- MTA .txt file downloads

---
+++ Test Python Code/Webscraping Test 1/Webscraping Test 1.py
@@ -0,0 +1,22 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+# connecting to website
+
+url = 'http://web.mta.info/developers/turnstile.html'
+response = requests.get(url)
+
+# Finding all of the hyperlink HTML tags
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+
+for i in range(36, len(soup.findAll('a'))+1):
+    one_a_tag = soup.findAll('a')[i]
+    link = one_a_tag['href']
+    download_url = 'http://web.mta.info/developers/' + link
+    urllib.request.urlretrieve(
+        download_url, './' + link[link.find('/turnstile_') + 1:])
+    time.sleep(1)
