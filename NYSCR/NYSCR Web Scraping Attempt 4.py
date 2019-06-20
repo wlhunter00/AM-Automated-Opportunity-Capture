331e190e3b98ed3780cde58751eb768419eed702
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2019-06-20 16:57:07 -0400

    cleaned more

--- NYSCR/NYSCR Web Scraping Attempt 4.py
+++ NYSCR/NYSCR Web Scraping Attempt 4.py
@@ -1,3 +1,4 @@
+# Test to see if GitHub works
 # Important imports
 import requests
 import time
@@ -10,6 +11,7 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Database=Opportunity Hunter;'
                       'Trusted_Connection=yes;')
 cursor = conn.cursor()
+
 
 def truncateSQL(tableName):
     cursor.execute('truncate table ' + tableName)
@@ -30,8 +32,8 @@ def findLastJob(tableName):
 # set above.
 def getURL(site, startingNumber):
     if(site == 'NYSCR'):
-        url = 'https://www.nyscr.ny.gov/adsOpen.cfm?startnum=' + startingNumber + '&orderBy=55&numPer=50&myAdsOnly=2&adClass=b&adCat=&adCounty=&adType=&mbe=0&wbe=0&dbe=0&keyword='
-    return url
+        urlFromFunction = 'https://www.nyscr.ny.gov/adsOpen.cfm?startnum=' + startingNumber + '&orderBy=55&numPer=50&myAdsOnly=2&adClass=b&adCat=&adCounty=&adType=&mbe=0&wbe=0&dbe=0&keyword='
+    return urlFromFunction
 
 
 def getContainers(site, startingNumber, HTMLobject, className):
@@ -68,9 +70,10 @@ def searchAndUpload(container, labelHTML, resultHMTL, labelDef, resultDef,
 
 
 # 1 and 50 are the pages we want to start at. str() casts these as strings
-def testFunction():
-    truncateSQL('NYSCRhybrid')
-    jobNumber = findLastJob('NYSCRhybrid')+1
+def scrapeSite(site, database, labelHTML, resultHMTL, labelDef, resultDef,
+               containerHTML, containerDef):
+    truncateSQL(database)
+    jobNumber = findLastJob(database)+1
     startNum = [str(1), str(50)]
     # For loop that goes through the array. Basically allows us to run multiple
     # pages
@@ -78,36 +81,18 @@ def testFunction():
         # The job_containers is the HTML element that encompases every job.
         # Allows us to run multiple containers
         # job_containers = soup.findAll('tr', class_='r1')
-        job_containers = getContainers('NYSCR', start, 'tr', 'r1')
+        job_containers = getContainers(site, start, 'tr', 'r1')
         # A for loop that goes through all of the containers and extracts the
         # info from the specific job. The way this is done will differ for each
         # website
         for container in job_containers:
             # Collecting the information from the container
             # Insert text block plus URL into SQL table
-            searchAndUpload(container, 'div', 'div', "labelText", "resultText",
-                            'NYSCRhybrid', jobNumber, start, 'NYSC')
-            # container_labels = container.findAll('div', class_="labelText")
-            # container_results = container.findAll('div', class_="resultText")
-            # for num in range(0, len(container_labels)):
-            #     cursor.execute('INSERT into NYSCRhybrid (jobID, labelText, resultText) VALUES (\''
-            #             + str(jobNumber).replace('\'','\'\'') + '\', \''
-            #                     + container_labels[num].text.replace('\'','\'\'') + '\',  \''
-            #                             + container_results[num].text.replace('\'','\'\'') + '\')')
-            #     conn.commit()
-            # cursor.execute('INSERT into NYSCRhybrid (jobID, labelText, resultText) VALUES (\''
-            #         + str(jobNumber).replace('\'','\'\'') + '\', \''
-            #                 + 'URL:' + '\',  \''
-            #                         + getURL('NYSCR', start).replace('\'','\'\'') + '\')')
-            # conn.commit()
-            # cursor.execute('INSERT into NYSCRhybrid (jobID, labelText, resultText) VALUES (\''
-            #         + str(jobNumber).replace('\'','\'\'') + '\', \''
-            #                 + 'dateInserted:' + '\',  \''
-            #                         + datetime.now().strftime('%Y-%m-%d %H:%M:%S').replace('\'','\'\'') + '\')')
-            # conn.commit()
+            searchAndUpload(container, labelHTML, resultHMTL, labelDef,
+                            resultDef, database, jobNumber, start, site)
             jobNumber += 1
         # 1 second delay to avoid overtaxing the server
         time.sleep(1)
 
 
-testFunction()
+scrapeSite('NYSCR', 'NYSCRhybrid', 'div', 'div', "labelText", "resultText", 'tr', 'r1')
