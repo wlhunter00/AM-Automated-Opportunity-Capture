8938cf4d645c4eac07f3fcd33fef335c4d5f33ac
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2019-06-26 17:23:36 -0400

    asd

--- GOVUK/GOVUK Webscraping 1.py
+++ GOVUK/GOVUK Webscraping 1.py
@@ -19,10 +19,13 @@ containers = soup.findAll('div', class_='search-result')
 # print(containers[1])
 
 # Title (url is in here too)
-title = containers[1].find('div', class_='search-result-header')
+title = containers[19].find('div', class_='search-result-header')
 link = title.find('a')['href']
-print(title.text)
-print(link)
+# print(title.text)
+# print(link)
+
+description = containers[0].find('span', class_='')
+print(description)
 
 company = containers[1].find('div', class_='search-result-sub-header wrap-text')
 
@@ -33,4 +36,4 @@ test = containers[1].findAll('div', class_='search-result-entry')
 for yeet in test:
     label.append(yeet.find('strong', class_='').text)
     result.append(yeet.find('strong', class_='').next_sibling)
-print(label[1], result[1])
+# print(label[1], result[1])
