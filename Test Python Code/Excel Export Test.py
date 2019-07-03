a638c4b0e3329910997cc90e4cc02b63cb1de863
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2019-07-03 13:43:55 -0500

    From sql to excel done

---
+++ Test Python Code/Excel Export Test.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import pyodbc
+from datetime import datetime
+
+# Connecting to SQL server
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=jackson;'
+                      'Database=OppHunter;'
+                      'Trusted_Connection=yes;')
+cursor = conn.cursor()
+
+script1 = "select * from current_table where status = 'new'"
+df1 = pd.read_sql_query(script1, conn)
+
+script2 = 'select * from current_table'
+df2 = pd.read_sql_query(script2, conn)
+
+script3 = 'select * from master_table'
+df3 = pd.read_sql_query(script3, conn)
+
+script4 = "select * from current_table where JobDescription like '%data%'"
+df4 = pd.read_sql_query(script4, conn)
+
+script5 = "select * from current_table where JobDescription like '%tech%'"
+df5 = pd.read_sql_query(script5, conn)
+
+script6 = "select * from current_table where JobDescription like '%legal%' or JobDescription like '%law%'"
+df6 = pd.read_sql_query(script6, conn)
+
+script7 = "select * from current_table where JobDescription like '%finance%'"
+df7 = pd.read_sql_query(script7, conn)
+
+with pd.ExcelWriter(r'C:\Users\whunter\Documents\GitHub\AM-Automated' +
+                     '-Oppurtinity-Capture\Excel Sheets\Results_' +
+                      datetime.now().strftime('%m-%d-%Y#%H%M') +
+                      '.xlsx') as writer:
+    df1.to_excel(writer, sheet_name='newly_added')
+    df2.to_excel(writer, sheet_name='current_table')
+    df3.to_excel(writer, sheet_name='master_table')
+    df4.to_excel(writer, sheet_name='data_related')
+    df5.to_excel(writer, sheet_name='tech_related')
+    df6.to_excel(writer, sheet_name='legal_related')
+    df7.to_excel(writer, sheet_name='finance_related')
