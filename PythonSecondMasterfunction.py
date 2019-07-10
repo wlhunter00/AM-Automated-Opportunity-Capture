63ab1d5dbd56db8b8a25e808bebdd3377f7b8a87
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2019-07-10 10:27:21 -0400

    smarter html

--- PythonSecondMasterfunction.py
+++ PythonSecondMasterfunction.py
@@ -19,6 +19,7 @@ sheets = []
 dataFrames = []
 dfForCount = []
 
+
 # Function that goes through text file and stores queries and sheets into
 # seperate lists.
 def splitKeyWordFile():
@@ -77,25 +78,25 @@ def sendEmail():
 
     # Stores string variables to be used in email.
     subject = 'Opportunity Hunter Daily Update'
-    test = 'x'
     body = 'Hello,\n\nThis is the daily Opportunity Hunter Report. Click the link to access the Excel Report.'
     html = ('<br><a href="https://alvarezandmarsal.box.com/s/hpchnqin29htdjpv0af8oyseilxl6vqc">Opportunity Hunter Report</a><br><br>' +
             '<p>Consider the table below for a quick update of the status of the table. <br>' +
             'Please respond to this email if you have any issues, or want to add any keywords. Please do not leave the table open for too long, as it needs to be closed everywhere for it to be updated.</p>' +
             '<table><tr><th></th><th>Newly Added</th><th>Current Table<th>Master Table</th><th>Data Related</th><th>Tech Related</th><th>Law Related</th><th>Finance Related</th></tr>' +
-            '<tr><td>New Additions</td><td align="center">' + test +
-            '</td><td align="center">' + test + '</td><td align="center">' + test +
-            '</td><td align="center">' + test + '</td><td align="center">' + test +
-            '</td><td align="center">' + test + '</td><td align="center">' + test +
-            '</td></tr>' + '<tr><td>Total Jobs</td><td align="center">' + test +
-            '</td><td align="center">' + test + '</td><td align="center">' + test +
-            '</td><td align="center">' + test + '</td><td align="center">' + test +
-            '</td><td align="center">' + test + '</td><td align="center">' + test +
+            '<tr><td>New Additions</td><td align="center">' + dataFrames[0].count(axis=0)[0] +
+            '</td><td align="center">' + dataFrames[0].count(axis=0)[0] + '</td><td align="center">' + dataFrames[0].count(axis=0)[0] +
+            '</td><td align="center">' + dfForCount[0].count(axis=0)[0] + '</td><td align="center">' + dfForCount[1].count(axis=0)[0] +
+            '</td><td align="center">' + dfForCount[2].count(axis=0)[0] + '</td><td align="center">' + dfForCount[3].count(axis=0)[0] +
+            '</td></tr>' + '<tr><td>Total Jobs</td><td align="center">' + dataFrames[0].count(axis=0)[0] +
+            '</td><td align="center">' + dataFrames[1].count(axis=0)[0] + '</td><td align="center">' + dataFrames[2].count(axis=0)[0] +
+            '</td><td align="center">' + dataFrames[3].count(axis=0)[0] + '</td><td align="center">' + dataFrames[4].count(axis=0)[0] +
+            '</td><td align="center">' + dataFrames[5].count(axis=0)[0] + '</td><td align="center">' + dataFrames[6].count(axis=0)[0] +
             '<br></td></tr></table><p>Thank You.</p>'
             )
     # connects to server and sends email.
     yag = yagmail.SMTP(senderEmail, password)
     yag.send(to=listAddresses, subject=subject, contents=[body, html])
+
 
 sendEmail()
 queryToExcelSheet()
